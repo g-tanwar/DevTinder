95d3407288631925481fb7be012fb61ab6eb00dd
g-tanwar <tanwarg282005@gmail.com>
2026-01-11 11:28:45 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,24 +2,24 @@ const express = require('express');
 
 const app =  express();
 
-app.use("/test",(req,res) => {                          // will handle any request starting from '/test' 
-    res.send("Hello from the Server !")
-})
+app.use("/user", (req,res,next) =>{
+      // route handler
 
-app.get("/user/:user_id",(req,res) =>{
-    console.log(req.params);
-    res.send({first_name:"Gourav", last_name:"Tanwar"})
-})
+    console.log("Handling route here...")  
+    next();
+    res.send("Route Hnandler 1...");
+    
+},
+(req,res) =>{
+    console.log("2nd handler here")
+    res.send("Route handler 2..");      
+          // should never give a multiple router
+}
+);
 
-app.delete("/user",(req,res) =>{
-    res.send("user deleted successfully")
-})
-app.post("/user",(req,res) =>{
-    console.log("Save data to the Database")
-    res.send("Data saved Successfully to the database")
-})
+
 app.listen(3000,()=>{
     console.log("server has been started successfully...")
 });
 
- 
+  
